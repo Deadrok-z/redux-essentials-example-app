65d6bdb7b1b1fd89f4695b3ac65b07c177e21c62
Deadrok-z <84705521+Deadrok-z@users.noreply.github.com>
2022-07-07 17:56:53 +0300

    Post Reaction Buttons

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -2,14 +2,47 @@ import { createSlice, nanoid } from '@reduxjs/toolkit'
 import { sub } from 'date-fns'
 
 const initialState = [
-  { id: '1', title: 'citation', content: 'Случай правит миром.', date: sub(new Date(1810, 5, 5), { minutes: 10}).toISOString(), user:"0" },
-  { id: '2', title: 'citation', content: 'Победить не берусь, перехитрить попробую.', date: sub(new Date(1812, 8, 16), { minutes: 10}).toISOString(), user:"2" }
+  {
+    id: '1',
+    title: 'citation',
+    content: 'Случай правит миром.',
+    date: sub(new Date(1810, 5, 5), { minutes: 10 }).toISOString(),
+    user: "0",
+    reactions: {
+      thumbsUp: 0,
+      hooray: 0,
+      heart: 0,
+      rocket: 0,
+      eyes: 0,
+    },
+  },
+  {
+    id: '2',
+    title: 'citation',
+    content: 'Победить не берусь, перехитрить попробую.',
+    date: sub(new Date(1812, 8, 16), { minutes: 10 }).toISOString(),
+    user: "2",
+    reactions: {
+      thumbsUp: 0,
+      hooray: 0,
+      heart: 0,
+      rocket: 0,
+      eyes: 0,
+    },
+  }
 ]
 
 const postsSlice = createSlice({
   name: 'posts',
   initialState,
   reducers: {
+    reactionAdded(state, action) {
+      const { postId, reaction } = action.payload
+      const existingPost = state.find(post => post.id === postId)
+      if (existingPost) {
+        existingPost.reactions[reaction]++
+      }
+    },
     postAdded: {
       reducer(state, action) {
         state.push(action.payload)
@@ -36,6 +69,7 @@ const postsSlice = createSlice({
     }
   }
 })
-export const { postAdded, postUpdated } = postsSlice.actions
+
+export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
