df5fcd299111c833d21325902b4094ea1fe5d44a
Deadrok-z <84705521+Deadrok-z@users.noreply.github.com>
2022-07-07 16:50:23 +0300

    Storing Dates for Posts

--- src/features/posts/SinglePostPage.js
+++ src/features/posts/SinglePostPage.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { Link } from 'react-router-dom'
 import { PostAuthor } from './PostAuthor'
+import { TimeAgo } from './TimeAgo'
 
 export const SinglePostPage = ({ match }) => {
   const { postId } = match.params
@@ -22,7 +23,12 @@ export const SinglePostPage = ({ match }) => {
     <section>
       <article className="post">
         <h2>{post.title}</h2>
-        <PostAuthor userId={post.user} />
+        <div>
+            <TimeAgo timestamp={post.date} />
+        </div>
+        <div>
+            <PostAuthor userId={post.user} />
+        </div>
         <p className="post-content">{post.content}</p>
         <Link to={`/editPost/${post.id}`} className="button">
           Edit Post
