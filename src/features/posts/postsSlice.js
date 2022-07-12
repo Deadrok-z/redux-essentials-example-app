4ad3c93f2a6d9e2db90f27209358d1c29ade1ded
Radmir <rrdark-job@mail.ru>
2022-07-12 16:20:59 +0300

    fix

--- src/features/posts/postsSlice.js
+++ src/features/posts/postsSlice.js
@@ -1,11 +1,10 @@
-import { createSlice, nanoid, createAsyncThunk } from '@reduxjs/toolkit'
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit'
 import { client } from '../../api/client'
-//import { sub } from 'date-fns'
 
 const initialState = {
   posts: [],
   status: 'idle',
-  error: null
+  error: null,
 }
 
 export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
@@ -13,36 +12,13 @@ export const fetchPosts = createAsyncThunk('posts/fetchPosts', async () => {
   return response.data
 })
 
-// const initialState = [
-//   {
-//     id: '1',
-//     title: 'citation',
-//     content: 'Случай правит миром.',
-//     date: sub(new Date(1810, 5, 5), { minutes: 10 }).toISOString(),
-//     user: "0",
-//     reactions: {
-//       thumbsUp: 0,
-//       hooray: 0,
-//       heart: 0,
-//       rocket: 0,
-//       eyes: 0,
-//     },
-//   },
-//   {
-//     id: '2',
-//     title: 'citation',
-//     content: 'Победить не берусь, перехитрить попробую.',
-//     date: sub(new Date(1812, 8, 16), { minutes: 10 }).toISOString(),
-//     user: "2",
-//     reactions: {
-//       thumbsUp: 0,
-//       hooray: 0,
-//       heart: 0,
-//       rocket: 0,
-//       eyes: 0,
-//     },
-//   }
-// ]
+export const addNewPost = createAsyncThunk(
+  'posts/addNewPost',
+  async (initialPost) => {
+    const response = await client.post('/fakeApi/posts', initialPost)
+    return response.data
+  }
+)
 
 const postsSlice = createSlice({
   name: 'posts',
@@ -50,35 +26,19 @@ const postsSlice = createSlice({
   reducers: {
     reactionAdded(state, action) {
       const { postId, reaction } = action.payload
-      const existingPost = state.posts.find(post => post.id === postId)
+      const existingPost = state.posts.find((post) => post.id === postId)
       if (existingPost) {
         existingPost.reactions[reaction]++
       }
     },
-    postAdded: {
-      reducer(state, action) {
-        state.posts.push(action.payload)
-      },
-      prepare(title, content, userId) {
-        return {
-          payload: {
-            id: nanoid(),
-            date: new Date().toISOString(),
-            title,
-            content,
-            user: userId
-          }
-        }
-      }
-    },
     postUpdated(state, action) {
       const { id, title, content } = action.payload
-      const existingPost = state.posts.find(post => post.id === id)
+      const existingPost = state.posts.find((post) => post.id === id)
       if (existingPost) {
         existingPost.title = title
         existingPost.content = content
       }
-    }
+    },
   },
   extraReducers(builder) {
     builder
@@ -94,14 +54,17 @@ const postsSlice = createSlice({
         state.status = 'failed'
         state.error = action.error.message
       })
-  }
+      .addCase(addNewPost.fulfilled, (state, action) => {
+        state.posts.push(action.payload)
+      })
+  },
 })
 
 export const { postAdded, postUpdated, reactionAdded } = postsSlice.actions
 
 export default postsSlice.reducer
 
-export const selectAllPosts = state => state.posts.posts
+export const selectAllPosts = (state) => state.posts.posts
 
 export const selectPostById = (state, postId) =>
-state.posts.posts.find(post => post.id === postId)
+  state.posts.posts.find((post) => post.id === postId)
