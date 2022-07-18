cb62b321f878652cdf8e3af799bdf852e3dd7a47
Deadrok-z <84705521+Deadrok-z@users.noreply.github.com>
2022-07-18 15:19:06 +0300

    Sorting Posts

--- src/features/posts/PostsList.js
+++ src/features/posts/PostsList.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useMemo } from 'react'
 import { Link } from 'react-router-dom'
 import { Spinner } from '../../components/Spinner'
 import { PostAuthor } from './PostAuthor'
@@ -26,19 +26,26 @@ let PostExcerpt = ({ post }) => {
 
 export const PostsList = () => {
   const {
-    data: posts,
+    data: posts = [],
     isLoading,
     isSuccess,
     isError,
     error
   } = useGetPostsQuery()
 
+  const sortedPosts = useMemo(() => {
+    const sortedPosts = posts.slice()
+    // Sort posts in descending chronological order
+    sortedPosts.sort((a, b) => b.date.localeCompare(a.date))
+    return sortedPosts
+  }, [posts])
+
   let content
 
   if (isLoading) {
     content = <Spinner text="Loading..." />
   } else if (isSuccess) {
-    content = posts.map(post => <PostExcerpt key={post.id} post={post} />)
+    content = sortedPosts.map(post => <PostExcerpt key={post.id} post={post} />)
   } else if (isError) {
     content = <div>{error}</div>
   }
